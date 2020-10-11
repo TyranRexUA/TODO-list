0174bf2d563c6410461c1a4eac4ff7bad0524a5a
TyranRexUA <tyranrexua@gmail.com>
2020-10-11 19:00:59 +0300

    Updates

---
+++ precache-manifest.913125c8bbcd1def5ac8a103d177e226.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1fdd19d29f3bbc9681aeb9df4a1fd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9a6700296c48205767f",
+    "url": "./static/css/main.000888e8.chunk.css"
+  },
+  {
+    "revision": "f92f018b3215a5ce20db",
+    "url": "./static/js/2.6331caaa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.6331caaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a6700296c48205767f",
+    "url": "./static/js/main.384f363f.chunk.js"
+  },
+  {
+    "revision": "37807aa12135f3f0df06",
+    "url": "./static/js/runtime-main.f6a946e7.js"
+  },
+  {
+    "revision": "d700ff9cac24ac4fc0d2a3b43adf323e",
+    "url": "./static/media/ComicNeue-Bold.d700ff9c.ttf"
+  },
+  {
+    "revision": "ee92f1df590e8358c4613d664350cba0",
+    "url": "./static/media/bg.ee92f1df.jpg"
+  },
+  {
+    "revision": "5cd302012939b7f5d1e1d12537de88ec",
+    "url": "./static/media/clear.5cd30201.svg"
+  },
+  {
+    "revision": "3de0086002950501f417e8a334801419",
+    "url": "./static/media/close.3de00860.svg"
+  },
+  {
+    "revision": "9d52ec89cc339fbf46ee24f73d679891",
+    "url": "./static/media/done.9d52ec89.svg"
+  },
+  {
+    "revision": "c6a92187bb664be91b740165dbd7dfcc",
+    "url": "./static/media/notDone.c6a92187.svg"
+  },
+  {
+    "revision": "aeda5b774e5c6d581e205fe284f8288c",
+    "url": "./static/media/plus.aeda5b77.svg"
+  }
+]);
